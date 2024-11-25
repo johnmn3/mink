5cd63e3afa3632df2bdd26c6390464e7677a08d7
John Michael Newman III <john.michael.newwman@gmail.com>
2024-11-24 23:34:13 -0500

    move off cljs.cli and onto cljs.repl for all 3 repls

--- src/mink/server.clj
+++ src/mink/server.clj
@@ -14,10 +14,12 @@
 (ns mink.server
   (:require
    [cljs.analyzer :as ana]
+   [cljs.build.api :as build]
+   [cljs.closure :as cljsc]
    [cljs.core.server :as server]
    [cljs.env :as env]
    [cljs.repl :as repl]
-   [mink.main :as otr]))
+   [mink.repl :as otr]))
 
 (defonce envs (atom {}))
 
@@ -39,14 +41,34 @@
                       (env/default-compiler-env)])))
     (get @envs k)))
 
+(defn refresh-ns []
+  (when (not= ana/*cljs-ns* (otr/current-ns))
+    (swap! otr/state assoc-in [:client->ns (otr/current-client)] (or ana/*cljs-ns* 'cljs.user))
+    nil))    
+
 (defn repl-prompt []
-  (print
-   (str
-    ana/*cljs-ns*
-    (when-let [client (otr/current-client)]
-      (when (< 1 client)
-        (str "/" client)))
-    "=> ")))
+  (let [current-client-id (otr/current-client)
+        current-repl-id (otr/repl-id)
+        dyn-ns ana/*cljs-ns*]
+    (refresh-ns)
+    (print (str "#_" dyn-ns ":" current-repl-id "/" current-client-id "=> "))))
+
+(defmulti repl-cmd (fn [first-two & _args] first-two))
+
+(defmethod repl-cmd "=>"
+  [ft rn res & _args]
+  (if (= ft "=>")
+    (let [n (read-string (apply str (drop 2 rn)))
+          dest-ns (or (get-in @otr/state [:client->ns n]) 'cljs.user)]
+      (set! ana/*cljs-ns* dest-ns)
+      (otr/set-client! n)
+      nil)
+    res))
+
+(defmethod repl-cmd "ls"
+  [_ft _rn _res & _args]
+  (println (otr/list-clients))
+  nil)
 
 (defn repl-read
   ([request-prompt request-exit]
@@ -54,25 +76,38 @@
   ([request-prompt request-exit opts]
    (let [res (repl/repl-read request-prompt request-exit opts)]
      (if (and (keyword? res) (= (namespace res) "mink.repl"))
-       (let [rn (name res)]
-         (if (.startsWith rn "=>")
-           (let [n (read-string (apply str (drop 2 rn)))]
-             (println "switching to client:" n)
-             (otr/set-client! n)
-             nil)
-           res))
+       (let [rn (name res)
+             first-two (apply str (take 2 rn))]
+         (repl-cmd first-two rn res))
        res))))
+
+(def wopts
+  {:watch-fn (fn [& _args]
+               (otr/websocket-eval "(function () {location.reload();})();"))
+   :watch "src"
+   :output-dir "out"
+   :asset-path "/out"
+   :prompt
+   repl-prompt})
 
 (defn repl
   ([]
    (repl nil))
-  ([{:keys [opts env-opts]}]
-   (let [[env cenv] (get-envs env-opts)]
+  ([{:as ctx :keys [opts env-opts]}]
+   (let [[env cenv] (get-envs env-opts)
+         known (select-keys env-opts cljsc/known-opts)]
      (env/with-compiler-env cenv
-       (repl/repl* env (assoc opts
-                              :prompt repl-prompt
-                              :read repl-read
-                              :watch "src"))))))
+       (build/build (str (:watch opts "src")) (merge wopts known))
+       (repl/repl* env
+                   (merge {:prompt repl-prompt
+                           :read repl-read
+                           :output-dir "out"
+                           :asset-path "/out"
+                           :watch "src"}
+                          opts
+                          wopts))))))
+#_(repl)
+
 
 (defn prepl
   ([]
